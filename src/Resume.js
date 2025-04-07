e3e8cd0c265c9913e0e4532f120556fbafb08242
Hiroaki0422 <kifa0422@gmail.com>
2025-04-07 14:00:40 -0700

    fix resume link

--- src/Resume.js
+++ src/Resume.js
@@ -1,27 +1,17 @@
 import React from "react";
-import resume from "./HIROAKI_OSHIMA.pdf";
-
-function download(filename, text1) {
-  const element = document.createElement("a");
-  const dataBlob = new Blob([text1], { type: "data:application/pdf;base64" });
-  const objectUrl = URL.createObjectURL(dataBlob);
-  element.href = objectUrl;
-  element.download = filename;
-  element.click();
-}
 
 const Resume = () => {
   return (
     <section id="resume">
-      <div class="flex-container2">
-        <div
-          style={{ cursor: "pointer", textDecoration: "underline" }}
-          onClick={() => {
-            download("Hiroaki_Oshima.pdf", resume);
-          }}
+      <div className="flex-container2">
+        <a
+          href="https://github.com/Hiroaki0422/web_portfolio/blob/main/src/HIROAKI_OSHIMA.pdf?raw=true"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "underline", cursor: "pointer" }}
         >
           Click to Download my latest resume
-        </div>
+        </a>
       </div>
     </section>
   );
